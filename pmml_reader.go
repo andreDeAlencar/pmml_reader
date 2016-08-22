d33aaa2622580c3861e20675a7bbd971b44fa149
André de Alencar <andre.alencar.p@gmail.com>
2016-08-22 09:43:50 -0300

    PMML reader for Go

---
+++ pmml_reader.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"io/ioutil"
+	"encoding/xml"
+)
+
+type Model struct {
+	PMML 
+}
+
+type PMML struct {
+	Version string `xml:"version,attr"`
+	XMLNs string `xml:"xmlns,attr"`
+	Header Header
+	DataDictionary DataDictionary
+}
+
+type Header struct {
+	Copyright string `xml:"copyright,attr"`
+	Application Application 
+}
+
+type Application struct {
+	Name string `xml:"name,attr"`
+	Version string `xml:"version,attr"`
+}
+
+type DataDictionary struct {
+	NumberOfFields int `xml:"numberOfFields,attr"`
+	DataFields []DataField `xml:"DataField"`
+}
+
+type DataField struct {
+	Name string `xml:"name,attr"`
+	Optype string `xml:"optype,attr"`
+	DataType string `xml:"dataType,attr"`
+	Interval Interval
+	Values []Value `xml:"Value"`
+}
+
+type Interval struct {
+	Closure string `xml:"closure,attr"`
+	LeftMargin float64 `xml:"leftMargin,attr"`
+	RightMargin float64 `xml:"rightMargin,attr"`
+}
+
+type Value struct {
+	Value string `xml:"value,attr"`
+}
+
+func main() {
+
+	file, err := os.Open(os.Args[1]) 
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	var p Model
+	
+	err = xml.Unmarshal(data, &p)
+    if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v", p)
+}
